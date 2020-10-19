754cda0d9238d17242db4f8ef22ab9a6e4653e24
Pavel Kokovin <pkokovin@mail.ru>
2020-10-19 15:40:24 +0300

    HW02_no_optional_avoid_npe

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -49,14 +49,17 @@ public class InMemoryMealRepository implements MealRepository {
 
     @Override
     public Collection<Meal> getAll(int userId) {
-        List<Meal> rezult = new ArrayList<>(repository.get(userId).values());
-        rezult.sort(new Comparator<Meal>() {
-            @Override
-            public int compare(Meal o1, Meal o2) {
-                return o2.getDate().compareTo(o2.getDate());
-            }
-        });
-        return repository.get(userId) != null ?  rezult : Collections.EMPTY_LIST;
+        List<Meal> rezult = new ArrayList<>(repository.get(userId) != null
+                ? repository.get(userId).values() : Collections.EMPTY_LIST);
+        if (rezult.size() > 0) {
+            rezult.sort(new Comparator<Meal>() {
+                @Override
+                public int compare(Meal o1, Meal o2) {
+                    return o2.getDate().compareTo(o1.getDate());
+                }
+            });
+        }
+        return repository.get(userId) != null ? rezult : Collections.EMPTY_LIST;
     }
 }
 
