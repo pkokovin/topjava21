d5bd2e4db307963778b7d73f8c7506f4ca456404
Pavel Kokovin <pkokovin@mail.ru>
2020-10-22 14:25:52 +0300

    HW03_03_no_optional

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -27,7 +27,10 @@ public class MealServlet extends HttpServlet {
 
     @Override
     public void init() {
-        springContext = new ClassPathXmlApplicationContext("spring/spring-app.xml", "spring/spring-db.xml");
+        springContext = new ClassPathXmlApplicationContext(
+                "spring/spring-app.xml",
+                "spring/spring-app-repo.xml",
+                "spring/spring-db.xml");
         mealController = springContext.getBean(MealRestController.class);
     }
 
